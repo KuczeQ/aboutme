632a83f6eba237f73d98b6d756573eeb39fcf84f
KuczeQ <65510168+KuczeQ@users.noreply.github.com>
2023-12-17 11:51:35 +0100

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,71 +1,57 @@
-
-var Clock = (function(){
-
-	var exports = function(element) {
-		this._element = element;
-		var html = '';
-		for (var i=0;i<6;i++) {
-			html += '<span>&nbsp;</span>';
-		}
-		this._element.innerHTML = html;
-		this._slots = this._element.getElementsByTagName('span');
-		this._tick();
+var Clock = (function () {
+	function pad(value) {
+	  return ('0' + value).slice(-2);
+	}
+  
+	function update(slot, value) {
+	  var now = slot.dataset.now;
+  
+	  if (!now) {
+		slot.dataset.now = value;
+		slot.dataset.old = value;
+		return;
+	  }
+  
+	  if (now !== value) {
+		flip(slot, value);
+	  }
+	}
+  
+	function flip(slot, value) {
+	  slot.classList.remove('flip');
+	  slot.dataset.old = slot.dataset.now;
+	  slot.dataset.now = value;
+	  slot.offsetLeft; // Trigger reflow
+	  slot.classList.add('flip');
+	}
+  
+	function tick() {
+	  var time = new Date();
+	  var timeString = pad(time.getHours()) + pad(time.getMinutes()) + pad(time.getSeconds());
+  
+	  for (var i = 0, l = this._slots.length; i < l; i++) {
+		update(this._slots[i], timeString.charAt(i));
+	  }
+  
+	  setTimeout(this._tick.bind(this), 1000);
+	}
+  
+	function Clock(element) {
+	  this._element = element;
+	  this._element.innerHTML = Array(6).fill('<span>&nbsp;</span>').join('');
+	  this._slots = this._element.getElementsByTagName('span');
+	  this._tick();
+	}
+  
+	Clock.prototype = {
+	  _tick: tick,
+	  _pad: pad,
 	};
-
-	exports.prototype = {
-
-		_tick:function() {
-			var time = new Date();
-			this._update(this._pad(time.getHours()) + this._pad(time.getMinutes()) + this._pad(time.getSeconds()));
-			var self = this;
-			setTimeout(function(){
-				self._tick();
-			},1000);
-		},
-
-		_pad:function(value) {
-			return ('0' + value).slice(-2);
-		},
-
-		_update:function(timeString) {
-
-			var i=0,l=this._slots.length,value,slot,now;
-			for (;i<l;i++) {
-
-				value = timeString.charAt(i);
-				slot = this._slots[i];
-				now = slot.dataset.now;
-
-				if (!now) {
-					slot.dataset.now = value;
-					slot.dataset.old = value;
-					continue;
-				}
-
-				if (now !== value) {
-					this._flip(slot,value);
-				}
-			}
-		},
-
-		_flip:function(slot,value) {
-
-			slot.classList.remove('flip');
-			slot.dataset.old = slot.dataset.now;
-			slot.dataset.now = value;
-
-			slot.offsetLeft;
-
-			slot.classList.add('flip');
-
-		}
-
-	};
-
-	return exports;
-}());
-
-var i=0,clocks = document.querySelectorAll('.clock'),l=clocks.length;
-for (;i<l;i++) {
-	new Clock(clocks[i]);
-}
+  
+	return Clock;
+  })();
+  
+  document.querySelectorAll('.clock').forEach(function (clockElement) {
+	new Clock(clockElement);
+  });
+  
