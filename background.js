6fba2519f29e2fd628150406ad19d950cc75571e
KuczeQ <65510168+KuczeQ@users.noreply.github.com>
2023-12-19 18:42:55 +0100

    Update background.js

--- background.js
+++ background.js
@@ -3,59 +3,57 @@
 
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
-    var width = canvas.width = window.innerWidth;
-    var height = canvas.height = window.innerHeight;
-    var mouseX = null;
-    var mouseY = null;
-    var distance = 80;
-    var smallerThan = Math.sqrt(distance * distance + distance * distance);
-    var mouseDistance = 150;
-    var numShapes;
+    var width, height, numShapes, smallerThan, mouseDistance, distance;
     var shapes = [];
+    var isWide, lineWidth, mouseX = null, mouseY = null;
     var easeFactor = 0.3;
     var friction = 0.9;
-    var lineWidth = 5;
-    var isWide = width > height;
-    numShapes = isWide ? width / distance : height / distance;
 
-    if (width < 768) {
-        lineWidth = 2;
-        distance = 40;
+    function initialize() {
+        width = canvas.width = window.innerWidth;
+        height = canvas.height = window.innerHeight;
+        isWide = width > height;
+        distance = isWide ? 100 : 10;
         smallerThan = Math.sqrt(distance * distance + distance * distance);
-        mouseDistance = 50;
-        numShapes = isWide ? width / distance : height / distance;
+        mouseDistance = isWide ? 140 : 100;
+        numShapes = isWide ? Math.floor(width / distance) : Math.floor(height / distance);
+
+        if (width < 768) {
+            lineWidth = 2;
+            distance = 40;
+            smallerThan = Math.sqrt(distance * distance + distance * distance);
+            mouseDistance = 50;
+            numShapes = isWide ? Math.floor(width / distance) : Math.floor(height / distance);
+        } else {
+            lineWidth = 5;
+        }
     }
 
-    window.requestAnimationFrame =
-        window.requestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        function (cb) {
-            setTimeout(cb, 17);
-        };
+    function requestAnimationFrame(cb) {
+        window.requestAnimationFrame =
+            window.requestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (cb) {
+                setTimeout(cb, 17);
+            };
 
-    function Shape(context, x, y, i) {
-        this.context = context;
-        this.initialize(x, y, i);
+        window.requestAnimationFrame(cb);
     }
 
-    Shape.prototype.initialize = function (x, y, i) {
+    function Shape(x, y, i, j) {
         this.x = x;
         this.y = y;
         this.xi = x;
         this.yi = y;
         this.i = i;
         this.r = 1;
-        this.v = {
-            x: 0,
-            y: 0
-        };
+        this.v = { x: 0, y: 0 };
         this.color = randomNum(0, 360);
-    };
+    }
 
     Shape.prototype.draw = function () {
-        var context = this.context;
         context.save();
         context.fillStyle = 'hsl(' + this.color + ', ' + '80%, 60%)';
         context.beginPath();
@@ -90,10 +88,9 @@
         }
     };
 
-    Shape.prototype.drawLines = function (i) {
-        var j = i;
+    Shape.prototype.drawLines = function () {
         for (var i = 0; i < shapes.length; i++) {
-            if (j !== i) {
+            if (this.i !== shapes[i].i || this.j !== shapes[i].j) {
                 var x = this.x - shapes[i].x;
                 var y = this.y - shapes[i].y;
                 var d = x * x + y * y;
@@ -112,60 +109,33 @@
         }
     };
 
-    Shape.prototype.render = function (i) {
-        this.drawLines(i);
+    Shape.prototype.initialize = function () {
+        this.drawLines();
         if (mouseX !== null) this.mouseDistance();
         this.draw();
     };
 
-    for (var i = 0; i < numShapes + 1; i++) {
-        for (var j = 0; j < numShapes + 1; j++) {
-            if (j * distance - distance > height) break;
-            var shape = new Shape(context, i * distance, j * distance, i, j);
-            shapes.push(shape);
-        }
-    }
-
     function render() {
         context.clearRect(0, 0, width, height);
         for (var i = 0; i < shapes.length; i++) {
-            shapes[i].render(i);
+            shapes[i].initialize();
         }
         requestAnimationFrame(render);
     }
 
-    render();
-
     function onResize() {
-        width = canvas.width = window.innerWidth;
-        height = canvas.height = window.innerHeight;
+        initialize();
         shapes = [];
-        if (width < 768) {
-            lineWidth = 2;
-            distance = 40;
-            smallerThan = Math.sqrt(distance * distance + distance * distance);
-            mouseDistance = 50;
-            numShapes = isWide ? width / distance : height / distance;
-        } else {
-            lineWidth = 5;
-            distance = 80;
-            smallerThan = Math.sqrt(distance * distance + distance * distance);
-            mouseDistance = 150;
-            numShapes = isWide ? width / distance : height / distance;
-        }
         for (var i = 0; i < numShapes + 1; i++) {
             for (var j = 0; j < numShapes + 1; j++) {
                 if (j * distance - distance > height) break;
-                var shape = new Shape(context, i * distance, j * distance, i, j);
+                var shape = new Shape(i * distance, j * distance, i, j);
                 shapes.push(shape);
             }
         }
     }
 
-    window.addEventListener('resize', function () {
-        onResize();
-    });
-
+    window.addEventListener('resize', onResize);
     window.addEventListener('mousemove', function (e) {
         mouseX = e.clientX;
         mouseY = e.clientY;
@@ -180,4 +150,15 @@
     function randomNum(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
+
+    initialize();
+    for (var i = 0; i < numShapes + 1; i++) {
+        for (var j = 0; j < numShapes + 1; j++) {
+            if (j * distance - distance > height) break;
+            var shape = new Shape(i * distance, j * distance, i, j);
+            shapes.push(shape);
+        }
+    }
+
+    render();
 })();
